36c89eac49b7ac6b91e9d5b5aee16ae947d9805a
Dusty211 <kylebenz@hotmail.com>
2019-01-13 02:01:30 -0500

    All tests passing.

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+
+class Post
+
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    return nil if self.author == nil
+    self.author.name
+  end
+
+end
